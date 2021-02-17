e735ed014d4e7fe8a6d3e2665389ba71739108e8
Ben <brbryant2639@gmail.com>
2021-02-17 17:52:51 -0600

    .

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
-const http = require("http");
 const fs = require("fs");
+const http = require("http");
 
 const port = 3000;
-
 // Creates a server object
 http
   .createServer(function (req, res) {
-    // Uses the readFile method from the fs module
-    fs.readFile("HelloWorld.txt", function (err, data) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      res.writeHead(200, { "Content-Type": "text/html" }); // http header
-      res.write(data); // write a response to the client
-      return res.end(); // end the response
-    });
+    res.writeHead(200, { "Content-Type": "text/html" }); // http header
+    console.log(req.params);
+    res.write(req.method); // write a response to the client
+    return res.end(); // end the response
   })
   .listen(port, () => console.log("Server listening on port: " + port));
